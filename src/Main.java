273264cb3dd8677abeef70af6aeca870fe5f214e
ahrian87 <maciej.zbydniewski@gmail.com>
2022-10-26 16:36:20 +0200

    brawo v1.0

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Podaj swoje imię: ");
+        String name = sc.nextLine();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Brawo " + name);
+        }
+    }
+}
